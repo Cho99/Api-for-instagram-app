5e859ecaa8101214673f72ebf9b66cecee9d3b23
Dog99 <quangsoi99@gmail.com>
2020-07-29 15:07:02 +0700

    Feature/Login

--- Api/user.controller.js
+++ Api/user.controller.js
@@ -1,9 +1,10 @@
 const User = require("../models/user.model");
-const shorid = require("shortid");
+const shortid = require("shortid");
+const jwt = require ("jsonwebtoken");
 
-const postRegister = function(req, res) {
-    req.body.id = shorid.generate();
-    User.create({
+const postRegister = async function(req, res) {
+    req.body.id = shortid.generate();
+    await User.create({
       id: req.body.id,
       username: req.body.username,
       email: req.body.email,
@@ -14,6 +15,33 @@ const postRegister = function(req, res) {
     console.log(req.body.password);
 }
 
+const postLogin = async function(req, res) {
+  var user = await User.findOne({
+    email: req.body.email
+  });
+  if(!user) {
+    return res.status(400).json({ error: "User not found" });
+  }
+  const data = {
+    email: req.body.email,
+    password: req.body.password
+  } 
+
+  if(user.password !== req.body.password) {
+    return res.status(400).json({ error: "Wrong password" });
+  }
+  console.log("Login success");
+  return jwt.sign({ user: data }, "123", { expiresIn: "30s" }, function(err, token){
+    res.json({ token, user });
+    console.log(res.json({ token, user })); 
+   })
+  
+  if(err) {
+    console.log(err)
+  }
+}
+
 module.exports = {
-    postRegister
+    postRegister,
+    postLogin
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-const { postRegister } = require("../Api/user.controller");
+const { postRegister, postLogin } = require("../Api/user.controller");
 
 router.route("/register")
     .post(postRegister);
+
+router.route("/login")
+    .post(postLogin);
 
 module.exports = router;
